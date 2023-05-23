ea71970983e168d8993a182e54a577a7b2ab4694
MarceloAggio <marceloaggio10@gmail.com>
2023-05-22 21:54:15 -0300

    att

--- js/scroll.js
+++ js/scroll.js
@@ -39,3 +39,24 @@ function scrollToIdOnClick(event) {
 
     });
 }
+
+const rodape = document.querySelectorAll('.mapa_rodape a[href^="#"]');
+
+rodape.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+
+    window.scroll({
+        top: to - 40,
+        behavior: "smooth"
+
+    });
+}
